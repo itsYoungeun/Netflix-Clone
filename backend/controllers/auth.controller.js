650c0dd8f1ae64ec5cd05eb6fe6c1dda804bc0a3
Peter <youngeunpc@yahoo.com>
2024-11-15 21:51:03 -0300

    Movie routes and controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 
 export async function signup(req, res) {
     try {
@@ -10,7 +12,7 @@ export async function signup(req, res) {
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if (emailRegex.test(email)) {
+        if (!emailRegex.test(email)) {
             return res.status(400).json({ success: false, message: "Invalid email" });
         }
 
@@ -30,17 +32,21 @@ export async function signup(req, res) {
 			return res.status(400).json({ success: false, message: "Username already exists" });
 		}
 
+        const salt = await bcryptjs.genSalt(10);
+		const hashedPassword = await bcryptjs.hash(password, salt);
+
         const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
 
 		const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
 
         const newUser = new User({
 			email,
-			password,
+			password: hashedPassword,
 			username,
 			image,
 		});
 
+        generateTokenAndSetCookie(newUser._id, res);
         await newUser.save();
 
         // Remove password from response
@@ -49,18 +55,55 @@ export async function signup(req, res) {
             user: {
             ...newUser._doc,
             password: "",
-        },
-    });
+            },
+        });
+
     } catch (error) {
         console.log("Error in signup controller", error.message);
         res.status(500).json({ success: false, message: "Internal server error" });
     }
-}
+};
 
 export async function login(req, res) {
-    res.send("Login route");
-}
+    try {
+		const { email, password } = req.body;
+
+		if (!email || !password) {
+			return res.status(400).json({ success: false, message: "All fields are required" });
+		}
+
+		const user = await User.findOne({ email: email });
+		if (!user) {
+			return res.status(404).json({ success: false, message: "Invalid credentials" });
+		}
+
+		const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+		if (!isPasswordCorrect) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			success: true,
+			user: {
+				...user._doc,
+				password: "",
+			},
+		});
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+};
 
 export async function logout(req, res) {
-    res.send("Logout route");
-}
+    try {
+		res.clearCookie("jwt-netflix");
+		res.status(200).json({ success: true, message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+};
