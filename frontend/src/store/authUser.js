770d7ab5abfe651fb9b7d99292ec01e10f682398
Peter <youngeunpc@yahoo.com>
2024-11-17 12:56:24 -0300

    Server api update

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -5,17 +5,20 @@ import { create } from "zustand";
 export const useAuthStore = create((set) => ({
     user: null,
     isSigningUp: false,
+    isCheckingAuth: true,
+	isLoggingOut: false,
+	isLoggingIn: false,
     signup: async (credentials) => {
-        set({ isSigningUp: true });
-        try {
-            const response = await axios.post("/api/v1/auth/signup", credentials);
-            set({ user: response.data.user, isSigningUp: false });
-            toast.success("Account created successfully");
-        } catch (error) {
-            toast.error(error.response.data.message || "An error occurred");
-            set({ isSigningUp: false, user: null });
-        }
-    },
+		set({ isSigningUp: true });
+		try {
+			const response = await axios.post("/api/v1/auth/signup", credentials);
+			set({ user: response.data.user, isSigningUp: false });
+			toast.success("Account created successfully");
+		} catch (error) {
+			toast.error(error.response.data.message || "Signup failed");
+			set({ isSigningUp: false, user: null });
+		}
+	},
     login: async () => {},
     logout: async () => {},
     authCheck: async () => {},
