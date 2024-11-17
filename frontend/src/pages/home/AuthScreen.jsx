298bbd8c669aff462198fc818a4d4584169a2b4f
Peter <youngeunpc@yahoo.com>
2024-11-17 01:39:27 -0300

    Auth Screen

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -16,12 +16,14 @@ const AuthScreen = () => {
         </header>
 
         {/* hero section */}
-
         <div className="flex flex-col items-center justify-center text-center py-40 text-white max-w-6xl mx-auto">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">Unlimited movies, TV shows, and more</h1>
-            <p className="text-lg mb-4">Watch anywhere. Cancel anytime.</p>
-            <p className="mb-4">Ready to watch? Enter your email to create or restart your membership.</p>
-
+            <p className="text-lg mb-4">
+                Watch anywhere. Cancel anytime.
+            </p>
+            <p className="mb-4">
+                Ready to watch? Enter your email to create or restart your membership.
+            </p>
             <form className="flex flex-col md:flex-row gap-4 w-1/2">
             <input
                 type="email"
@@ -45,7 +47,9 @@ const AuthScreen = () => {
             <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
                 {/* Left side */}
                 <div className="flex-1 text-center md:text-left">
-                    <h2 className="text-4xl md:text-5xl font-extrabold mb-4">Enjoy on your TV</h2>
+                    <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+                        Enjoy on your TV
+                    </h2>
                     <p className="text-lg md:text-xl">
                         Watch on Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blue-ray players, and more.
                     </p>
@@ -71,7 +75,7 @@ const AuthScreen = () => {
 
         {/* 2nd section */}
         <div className="py-10 bg-black text-white">
-            <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+            <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2">
                 {/* Left side */}
                 <div className="flex-1">
                     <div className="relative">
@@ -80,8 +84,12 @@ const AuthScreen = () => {
                             <img src="/stranger-things-sm.png" alt="Image" className="h-full" />
                             <div className="flex justify-between items-center w-full">
                                 <div className="flex flex-col gap-0">
-                                    <span className="text-md lg:text-lg font-bold">Stranger Things</span>
-                                    <span className="text-sm text-blue-500">Downloading...</span>
+                                    <span className="text-md lg:text-lg font-bold">
+                                        Stranger Things
+                                    </span>
+                                    <span className="text-sm text-blue-500">
+                                        Downloading...
+                                    </span>
                                 </div>
                                 <img src="/download-icon.gif" alt="" className="h-12" />
                             </div>
@@ -89,7 +97,68 @@ const AuthScreen = () => {
                     </div>
                 </div>
                 {/* Right side */}
-                <div className="flex-1">right</div>
+                <div className="flex-1 md:text-left text-center">
+                    <h2 className="text-4xl md:text-5xl font-extrabold mb-4 text-balance">
+                        Download our shows to watch offline
+                    </h2>
+                    <p className="text-lg md:text-xl">
+                        Save your favorites easily and always have something to watch.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        {/* separator */}
+        <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+        {/* 3rd section */}
+        <div className="py-10 bg-black text-white">
+            <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+                {/* Left side */}
+                <div className="flex-1 text-center md:text-left">
+                    <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+                        Watch everywhere
+                    </h2>
+                    <p className="text-lg md:text-xl">
+                        Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.
+                    </p>
+                </div>
+                {/* Right side */}
+                <div className="flex-1 relative overflow-hidden">
+                    <img src="/device-pile.png" alt="Device image" className="mt-4 z-20 relative" />
+                    <video 
+                        className="absolute top-2 left-1/2 -translate-x-1/2 h-4/6 z-10 max-w-[63%]"
+                        playsInline
+                        autoPlay={true}
+                        muted
+                        loop
+                    >
+                        <source src="/video-devices.m4v" type="video/mp4" />
+                    </video>
+                </div>
+            </div>
+        </div>
+
+        {/* separator */}
+        <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+        {/* 4th section */}
+        <div className="py-10 bg-black text-white">
+            <div className="flex max-w-6xl mx-auto items-center justify-center flex-col-reverse md:flex-row px-4 md:px-2">
+                {/* Left */}
+                <div className="flex-1 relative">
+                    <img src="/kids.png" alt="Enjoy on your TV" className="mt-4" />
+                </div>
+                {/* Rightt */}
+                <div className="flex-1 text-center md:text-left">
+                    <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+                        Create profiles for kids
+                    </h2>
+                    <p className="text-lg md:text-xl">
+                        Send kids on adventures with their favorite characters in a space made just 
+                        for them-free with your membership.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
