2851e4aec80d8827ebe60e5e1e0eca320a97f58d
Peter <youngeunpc@yahoo.com>
2024-11-19 22:49:42 -0300

    Searchhistory and 404

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/404";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
@@ -32,10 +34,12 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={!user ? <LoginPage/> : <Navigate to={"/"} />} />
-        <Route path="/signup" element={!user ? <SignUpPage/> : <Navigate to={"/"} />} />
-        <Route path="/watch/:id" element={user ? <WatchPage/> : <Navigate to={"/login"} />} />
-        <Route path="/search" element={user ? <SearchPage/> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
+        <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+        <Route path="/search" element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path="/history" element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
       <Toaster />
